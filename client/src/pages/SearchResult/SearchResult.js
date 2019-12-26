e709946f07638f55ad1d88612c6c4f5e5a3bb882
Ana Chernov <anastasia.chernov@gmail.com>
2019-12-26 14:55:18 -0500

    info window

--- client/src/pages/SearchResult/SearchResult.js
+++ client/src/pages/SearchResult/SearchResult.js
@@ -47,12 +47,27 @@ class SearchResult extends Component {
       zoom: 16
     });
 
+    // Create An InfoWindow
+    var infowindow = new window.google.maps.InfoWindow();
+
+    // We will need to change this
+    var contentString = this.state.address;
+
     var marker = new window.google.maps.Marker({
       position: {
         lat: this.state.latitude,
         lng: this.state.longitude
       },
       map: map
+    });
+
+    // Click on A Marker!
+    marker.addListener("click", function() {
+      // Change the content
+      infowindow.setContent(contentString);
+
+      // Open An InfoWindow
+      infowindow.open(map, marker);
     });
   };
 
