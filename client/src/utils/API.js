e461bdd152423ec29dfcbac87df98e08bba91611
Valentine <abraval@gmail.com>
2019-12-27 02:22:00 -0500

    Listings are displaying on profile page

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,11 @@ import axios from "axios";
 export default {
   saveListing: function(listingData) {
     return axios.post("/api/listing", listingData);
+  },
+  getUser: function () {
+    return axios.get("/user/");
+  },
+  getListings: function(){
+    return axios.get("/api/listing");
   }
 };
