f9dc7f6734829013eb1df70a20e311a47b01866f
Ana Chernov <anastasia.chernov@gmail.com>
2020-01-02 14:49:09 -0500

    Fixed issue where dialog was not opening

--- client/src/components/ListingCard/index.js
+++ client/src/components/ListingCard/index.js
@@ -3,7 +3,12 @@ import "./style.css";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import FormDialog from "../Dialog";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const styles = theme => ({
   button: {
@@ -14,44 +19,76 @@ const styles = theme => ({
   }
 });
 
-function ListingCard(props) {
-  const { classes } = props;
+class ListingCard extends React.Component {
+  state = {
+    open: false
+  };
 
-  return (
-    <div className="card" {...props} tabIndex="0">
-      <div className="card-header">
-        <div className="row">
-          <div className="card-title">{props.title}</div>
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div className="card" {...this.props} tabIndex="0">
+        <div className="card-header">
+          <div className="row">
+            <div className="card-title">{this.props.title}</div>
+          </div>
+        </div>
+        <div className="card-body">
+          <img className="photo img-fluid" src={this.props.photo} />
+          <p className="adress">{this.props.address}</p>
+          <p className="city">{this.props.city}</p>
+          <p className="city">{this.props.state}</p>
+          <p className="zipcode">{this.props.zipcode}</p>
+        </div>
+
+        <div className="card-footer">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.handleClickOpen()}
+          >
+            Edit Listing
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={event => this.props.handleAvailListing(event)}
+          >
+            Edit Availability
+          </Button>
+          <Dialog open={this.state.open} handleClickOpen={this.handleClickOpen}>
+            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address
+                here. We will send updates occasionally.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Email Address"
+                type="email"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => this.handleClose()} color="primary">
+                Cancel
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
-      <div className="card-body">
-        <img className="photo img-fluid" src={props.photo} />
-        <p className="adress">{props.address}</p>
-        <p className="city">{props.city}</p>
-        <p className="city">{props.state}</p>
-        <p className="zipcode">{props.zipcode}</p>
-      </div>
-
-      <div className="card-footer">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => props.handleClickOpen()}
-        >
-          Edit Listing
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={event => props.handleAvailListing(event)}
-        >
-          Edit Availability
-        </Button>
-      </div>
-
-      <FormDialog></FormDialog>
-    </div>
-  );
+    );
+  }
 }
 
 export default ListingCard;
