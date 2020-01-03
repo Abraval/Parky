4cd84caef6c34f9f8c2c30851f67f13cac52c066
Ana Chernov <anastasia.chernov@gmail.com>
2020-01-03 13:11:46 -0500

    Grid layout for profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -5,7 +5,24 @@ import ReservCard from "../../components/ReservCard";
 import API from "../../utils/API";
 import "./style.css";
 import Nav from "../../components/Nav";
+// Material UI Grid Layout imports
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 1,
+    margin: "auto",
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    height: "100%"
+  }
+});
 
 class Profile extends Component {
   state = {
@@ -23,7 +40,7 @@ class Profile extends Component {
     API.getUser()
       .then(res => {
         console.log("=======");
-        console.log (res)
+        console.log(res);
         console.log("=======");
         this.setState({ user: res.data });
         this.setState({ userId: res.data.user._id });
@@ -43,30 +60,6 @@ class Profile extends Component {
     console.log("testing user");
     console.log(this.state);
   }
-
-  // handleEditListing = event => {
-  //   event.preventDefault();
-
-  //   console.log("This is edit message!");
-  // };
-
-  // handleAvailListing = event => {
-  //   event.preventDefault();
-
-  //   console.log("this is availability message!");
-  // };
-
-  //  handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  //  handleClose = () => {
-  //   setOpen(false);
-  // };
-
-  //  handleOpen2 = () => {
-  //   setOpen(true);
-  // };
 
   loadListings = () => {
     API.getListingsForProf()
@@ -95,68 +88,142 @@ class Profile extends Component {
   };
 
   render() {
-    console.log("##################");
-    console.log(this.state.listing.length);
-    console.log(
-      this.state.listing.filter(item =>
-        console.log(item.user === this.state.userId)
-      )
-    );
-    console.log(this.state.userId);
+    const { classes } = this.props;
+    const { spacing } = this.state;
     return (
       <div>
-        {console.log(this.state.listing)}
         <Nav />
-        {/* <h2>Welcome back, {this.state.userName}!</h2> */}
-
-        {this.state.listing
-          // .filter(listing => listing.user._id === this.state.user.user._id)
-          .map(listing => {
-            if (listing.user === this.state.userId) {
-              return (
+        <div className={classes.root}>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              <Paper className={classes.paper}>
+                Sidebar & Profile Image
+                <h2>Welcome back, {this.state.userName}!</h2>
+              </Paper>
+            </Grid>
+            {/* ////////////////////////////// */}
+            <Grid item xs={10}>
+              <Paper className={classes.paper}>
+                Main Content
                 <div>
                   <h1>LISTINGS</h1>
-                  <ListingCard
-                    key={listing._id}
-                    id={listing._id}
-                    title={listing.title}
-                    photo={listing.photo}
-                    address={listing.address}
-                    city={listing.city}
-                    state={listing.state}
-                    zipcode={listing.zipcode}
-                    handleEditListing={this.handleEditListing}
-                    handleAvailListing={this.handleAvailListing}
-                  />
+                  {this.state.listing
+                    // .filter(listing => listing.user._id === this.state.user.user._id)
+                    .map(listing => {
+                      if (listing.user === this.state.userId) {
+                        return (
+                          <div>
+                            <ListingCard
+                              key={listing._id}
+                              id={listing._id}
+                              title={listing.title}
+                              photo={listing.photo}
+                              address={listing.address}
+                              city={listing.city}
+                              state={listing.state}
+                              zipcode={listing.zipcode}
+                              handleEditListing={this.handleEditListing}
+                              handleAvailListing={this.handleAvailListing}
+                            />
+                          </div>
+                        );
+                      }
+                    })}
+
+                  <h1>RESERVATIONS</h1>
+                  {this.state.reserved.map(reserved => {
+                    if (reserved.renter === this.state.userId)
+                      // {this.state.listings.map(listing) => {
+                      //   if(listing._id === reserved.listing)
+
+                      return (
+                        <div>
+                          <ReservCard
+                            date={moment(reserved.date).format("LL")}
+                            address={reserved.address}
+                            title={reserved.title}
+                            photo={reserved.photo}
+                          />
+                        </div>
+                      );
+                  })}
+                  {/* })} */}
                 </div>
-              );
-            }
-          })}
-
-        {this.state.reserved.map(reserved => {
-          if (reserved.renter === this.state.userId)
-            // {this.state.listings.map(listing) => {
-            //   if(listing._id === reserved.listing)
-
-            return (
-              <div>
-                <h1>RESERVATIONS</h1>
-
-                <ReservCard
-
-              
-                  date = {moment(reserved.date).format('LL')}
-                  address={reserved.address}
-                  title={reserved.title}
-                  photo={reserved.photo}
-                />
-              </div>
-            );
-        })}
-        {/* })} */}
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+//   render() {
+//     console.log("##################");
+//     console.log(this.state.listing.length);
+//     console.log(
+//       this.state.listing.filter(item =>
+//         console.log(item.user === this.state.userId)
+//       )
+//     );
+//     console.log(this.state.userId);
+//     return (
+//       <div>
+//         {console.log(this.state.listing)}
+//         <Nav />
+//         {/* <h2>Welcome back, {this.state.userName}!</h2> */}
+
+//         {this.state.listing
+//           // .filter(listing => listing.user._id === this.state.user.user._id)
+//           .map(listing => {
+//             if (listing.user === this.state.userId) {
+//               return (
+//                 <div>
+//                   <h1>LISTINGS</h1>
+//                   <ListingCard
+//                     key={listing._id}
+//                     id={listing._id}
+//                     title={listing.title}
+//                     photo={listing.photo}
+//                     address={listing.address}
+//                     city={listing.city}
+//                     state={listing.state}
+//                     zipcode={listing.zipcode}
+//                     handleEditListing={this.handleEditListing}
+//                     handleAvailListing={this.handleAvailListing}
+//                   />
+//                 </div>
+//               );
+//             }
+//           })}
+
+//         {this.state.reserved.map(reserved => {
+//           if (reserved.renter === this.state.userId)
+//             // {this.state.listings.map(listing) => {
+//             //   if(listing._id === reserved.listing)
+
+//             return (
+//               <div>
+//                 <h1>RESERVATIONS</h1>
+
+//                 <ReservCard
+
+//                   date = {moment(reserved.date).format('LL')}
+//                   address={reserved.address}
+//                   title={reserved.title}
+//                   photo={reserved.photo}
+//                 />
+//               </div>
+//             );
+//         })}
+//         {/* })} */}
+//       </div>
+//     );
+//   }
+// }
+
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Profile);
